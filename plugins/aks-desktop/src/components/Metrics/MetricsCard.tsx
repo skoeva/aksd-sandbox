Azure/aks-desktop

aks-plugin: MetricsCard: Fix Deploy/Deployment typing errors

plugins/aks-desktop/src/components/Metrics/MetricsCard.tsx
@@ -3,6 +3,7 @@
 
 import { Icon } from '@iconify/react';
 import { K8s } from '@kinvolk/headlamp-plugin/lib';
+import Deployment from '@kinvolk/headlamp-plugin/lib/lib/k8s/deployment';
 import {
   Box,
   CircularProgress,
@@ -30,7 +31,7 @@ interface MetricsCardProps {
   project: Project;
 }
 
-interface Deployment {
+interface Deploy {
   name: string;
   namespace: string;
 }
@@ -59,7 +60,7 @@ function formatMemory(bytes: number): string {
 
 function MetricsCard({ project }: MetricsCardProps) {
   const [selectedDeployment, setSelectedDeployment] = useState<string>('');
-  const [deployments, setDeployments] = useState<Deployment[]>([]);
+  const [deploys, setDeploys] = useState<Deploy[]>([]);
   const [metrics, setMetrics] = useState<MetricData>({
     cpuUsage: 'N/A',
     memoryUsage: 'N/A',
@@ -93,25 +94,25 @@ function MetricsCard({ project }: MetricsCardProps) {
 
     try {
       const cancel = K8s.ResourceClasses.Deployment.apiList(
-        (deploymentList: K8s.Deployment[]) => {
-          const deployments = deploymentList
-            .filter((deployment: K8s.Deployment) => deployment.getNamespace() === namespace)
-            .map((deployment: K8s.Deployment) => ({
+        (deploymentList: Deployment[]) => {
+          const deploysInNamespace: Deploy[] = deploymentList
+            .filter((deployment: Deployment) => deployment.getNamespace() === namespace)
+            .map((deployment: Deployment) => ({
               name: deployment.getName(),
               namespace: deployment.getNamespace(),
             }));
 
-          setDeployments(deployments);
+          setDeploys(deploysInNamespace);
 
-          if (deployments.length > 0 && !selectedDeployment) {
-            setSelectedDeployment(deployments[0].name);
+          if (deploysInNamespace.length > 0 && !selectedDeployment) {
+            setSelectedDeployment(deploysInNamespace[0].name);
           }
           setLoading(false);
         },
         (error: any) => {
           console.error('MetricsCard: Error fetching deployments:', error);
           setError('Failed to fetch deployments');
-          setDeployments([]);
+          setDeploys([]);
           setLoading(false);
         },
         {
@@ -301,17 +302,17 @@ function MetricsCard({ project }: MetricsCardProps) {
             value={selectedDeployment || ''}
             onChange={handleDeploymentChange}
             label="Select Deployment"
-            disabled={loading || deployments.length === 0}
+            disabled={loading || deploys.length === 0}
           >
             {loading ? (
               <MenuItem disabled>
                 <CircularProgress size={16} style={{ marginRight: 8 }} />
                 Loading deployments...
               </MenuItem>
-            ) : deployments.length === 0 ? (
+            ) : deploys.length === 0 ? (
               <MenuItem disabled>No deployments found</MenuItem>
             ) : (
-              deployments.map(deployment => (
+              deploys.map(deployment => (
                 <MenuItem key={deployment.name} value={deployment.name}>
                   {deployment.name}
                 </MenuItem>

skoeva/aksd-sandbox

plugins/aks-desktop/src/components/Metrics/MetricsCard.tsx
@@ -3,6 +3,7 @@
 
 import { Icon } from '@iconify/react';
 import { K8s } from '@kinvolk/headlamp-plugin/lib';
+import Deployment from '@kinvolk/headlamp-plugin/lib/lib/k8s/deployment';
 import {
   Box,
   CircularProgress,
@@ -30,7 +31,7 @@ interface MetricsCardProps {
   project: Project;
 }
 
-interface Deployment {
+interface DeploymentInfo {
   name: string;
   namespace: string;
 }
@@ -59,7 +60,7 @@ function formatMemory(bytes: number): string {
 
 function MetricsCard({ project }: MetricsCardProps) {
   const [selectedDeployment, setSelectedDeployment] = useState<string>('');
-  const [deployments, setDeployments] = useState<Deployment[]>([]);
+  const [deployments, setDeployments] = useState<DeploymentInfo[]>([]);
   const [metrics, setMetrics] = useState<MetricData>({
     cpuUsage: 'N/A',
     memoryUsage: 'N/A',
@@ -93,10 +94,10 @@ function MetricsCard({ project }: MetricsCardProps) {
 
     try {
       const cancel = K8s.ResourceClasses.Deployment.apiList(
-        (deploymentList: K8s.Deployment[]) => {
-          const deployments = deploymentList
-            .filter((deployment: K8s.Deployment) => deployment.getNamespace() === namespace)
-            .map((deployment: K8s.Deployment) => ({
+        (deploymentList: Deployment[]) => {
+          const deployments: DeploymentInfo[] = deploymentList
+            .filter((deployment: Deployment) => deployment.getNamespace() === namespace)
+            .map((deployment: Deployment) => ({
               name: deployment.getName(),
               namespace: deployment.getNamespace(),
             }));