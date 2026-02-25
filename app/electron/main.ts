Azure/aks-desktop

aks-desktop: app: Cache shell environment at startup for improved command execution
This improves performance by avoiding repeated shell environment
queries and ensures consistent environment variables across commands.

Co-authored-by: Santhosh Nagaraj <sannagaraj@microsoft.com>

Signed-off-by: Joaquim Rocha <joaquim.rocha@microsoft.com>

app/electron/main.ts
@@ -599,7 +599,7 @@ async function getShell(): Promise<string> {
  * Retrieves the environment variables from the user's shell.
  * @returns A promise that resolves to the shell environment.
  */
-async function getShellEnv(): Promise<NodeJS.ProcessEnv> {
+export async function getShellEnv(): Promise<NodeJS.ProcessEnv> {
   const execPromisify = promisify(exec);
   const shell = await getShell();
   const isWindows = process.platform === 'win32';
@@ -1324,8 +1324,32 @@ function adjustZoom(delta: number) {
   setZoom(newZoom);
 }
 
-function startElecron() {
+// Global variable to store the shell environment
+let shellEnvironment: NodeJS.ProcessEnv | null = null;
+
+/**
+ * Gets the shell environment that was initialized at startup.
+ * Falls back to process.env if not initialized.
+ */
+export function getShellEnvironment(): NodeJS.ProcessEnv {
+  return shellEnvironment || process.env;
+}
+
+async function initializeShellEnvironment() {
+  try {
+    shellEnvironment = await getShellEnv();
+    console.log('Shell environment initialized with PATH:', shellEnvironment.PATH);
+  } catch (error) {
+    console.warn('Failed to get shell environment, using process.env:', error);
+    shellEnvironment = process.env;
+  }
+}
+
+async function startElecron() {
   console.info('App starting...');
+
+  // Initialize shell environment once at startup
+  await initializeShellEnvironment();
 
   let appVersion: string;
   if (isDev && process.env.HEADLAMP_APP_VERSION) {

skoeva/aksd-sandbox

app/electron/main.ts
@@ -599,7 +599,7 @@ async function getShell(): Promise<string> {
  * Retrieves the environment variables from the user's shell.
  * @returns A promise that resolves to the shell environment.
  */
-async function getShellEnv(): Promise<NodeJS.ProcessEnv> {
+export async function getShellEnv(): Promise<NodeJS.ProcessEnv> {
   const execPromisify = promisify(exec);
   const shell = await getShell();
   const isWindows = process.platform === 'win32';
@@ -1329,13 +1329,37 @@ function adjustZoom(delta: number) {
   setZoom(newZoom);
 }
 
-function startElectron() {
+// Global variable to store the shell environment
+let shellEnvironment: NodeJS.ProcessEnv | null = null;
+
+/**
+ * Gets the shell environment that was initialized at startup.
+ * Falls back to process.env if not initialized.
+ */
+export function getShellEnvironment(): NodeJS.ProcessEnv {
+  return shellEnvironment || process.env;
+}
+
+async function initializeShellEnvironment() {
+  try {
+    shellEnvironment = await getShellEnv();
+    console.log('Shell environment initialized with PATH:', shellEnvironment.PATH);
+  } catch (error) {
+    console.warn('Failed to get shell environment, using process.env:', error);
+    shellEnvironment = process.env;
+  }
+}
+
+async function startElectron() {
   console.info('App starting...');
 
   // Increase max listeners to prevent false positive warnings
   // The app legitimately needs multiple IPC listeners (currently 11)
   // Default is 10, setting to 20 provides headroom for future additions
   ipcMain.setMaxListeners(20);
+
+  // Initialize shell environment once at startup
+  await initializeShellEnvironment();
 
   let appVersion: string;
   if (isDev && process.env.HEADLAMP_APP_VERSION) {